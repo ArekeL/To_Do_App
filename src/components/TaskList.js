769cc05ac475b95d38aa85eb6ff63b3009805300
Arek Lechnik <alechnik@gmail.com>
2023-01-04 05:31:40 +0100

    Sort active tasks from A to Z and done by date

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,6 +3,35 @@ import Task from "./Task";
 const TaskList = (props) => {
 	const active = props.tasks.filter((task) => task.active);
 	const done = props.tasks.filter((task) => !task.active);
+
+	// method - sort() 1
+	// done.sort((a, b) => b.finishDate - a.finishDate);
+
+	// method - sort() 2
+	done.sort((a, b) => {
+		if (a.finishDate < b.finishDate) {
+			return 1;
+		}
+		if (a.finishDate > b.finishDate) {
+			return -1;
+		}
+		return 0;
+	});
+
+	if (active.length > 2) {
+		active.sort((a, b) => {
+			a = a.text.toLowerCase();
+			b = b.text.toLowerCase();
+			if (a < b) {
+				return -1;
+			}
+			if (a > b) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+
 	const activeTasks = active.map((task) => (
 		<Task
 			key={task.id}
